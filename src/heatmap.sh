ab9b6bbda453bbfeab0c7fd2a5485a8c0fd4e6f8
Jingqi <jingqliu@umich.edu>
2017-07-20 10:51:36 -0400

    Update heatmap.sh

--- src/heatmap.sh
+++ src/heatmap.sh
@@ -1,1 +1,8 @@
-
+shp2json ne_50m_admin_0_countries.shp -o world.json
+geoproject 'd3.geoEquirectangular().rotate([100,0]).center([-50,0]).fitSize([1800,900],d)' < world.json > world-project.json
+ndjson-split 'd.features' < world-project.json > world-project.ndjson
+ndjson-map 'd.id = d.properties.iso_a2, d' < world-project.ndjson > world-project1.ndjson
+ndjson-join 'd.id' world-project1.ndjson count_final.ndjson > x.ndjson
+ndjson-map -r d3=d3-array 'd[0].properties = {density: d3.bisect([1,10,50,200,500,1000,2000,4000],(d[1].Count))}, d[0]' < x.ndjson >xx.ndjson
+ndjson-map -r d3=d3-scale-chromatic '(d.properties.fill = d3.schemeYlGnBu[9][d.properties.density], d)' < xx.ndjson > xxx.ndjson
+geo2svg -n --stroke '#CCC' -p 1 -w 1800 -h 900 < xxx.ndjson > xxx1800.svg   
