14bdaa5dcc0ab1105b8a5032dd126bd7b4e2e5c6
Jingqi <jingqliu@umich.edu>
2017-07-24 13:38:19 -0400

    Update heatmap.sh

--- src/heatmap.sh
+++ src/heatmap.sh
@@ -6,3 +6,8 @@ ndjson-join 'd.id' world-project1.ndjson count_final.ndjson > x.ndjson
 ndjson-map -r d3=d3-array 'd[0].properties = {density: d3.bisect([1,10,50,200,500,1000,2000,4000],(d[1].Count))}, d[0]' < x.ndjson >xx.ndjson
 ndjson-map -r d3=d3-scale-chromatic '(d.properties.fill = d3.schemeYlGnBu[9][d.properties.density], d)' < xx.ndjson > xxx.ndjson
 geo2svg -n --stroke '#CCC' -p 1 -w 1800 -h 900 < xxx.ndjson > xxx1800.svg   
+
+
+#For interactive
+ndjson-map -r d3=d3-array 'd[0].properties = {densityscale: d3.bisect([1,10,50,200,500,1000,2000,4000],(d[1].Count)),density: d[1].Count}, d[0]' < x.ndjson >xx.ndjson
+geo2topo -n counties=xx.ndjson > topo.json
